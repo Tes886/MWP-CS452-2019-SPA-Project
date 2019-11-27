3a66e106e388efade1a9f8414ef86a5bf91696ff
Tesfaye Abie <tesfayedds30@gmail.com>
2019-11-27 14:52:35 -0600

    Updated day 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// your code here
+
+
+const login = `
+                <h1>Please Login</h1><br>
+                Username:<input placeholder="mwp" value="mwp"/><br/>
+                Password:<input placeholder="123" value="123"/><br/>
+                <button id="loginbtn">Login</button>
+            `
+let login = document.getElementById("outlet");
+login.innerHTML = login;
+document.getElementById("loginbtn").addEventListener("click", login)
+
+const animation = `
+                    <div id="address"> Welcome to SPA Animation</div>
+                    <textarea id="animation" rows="25" cols="25" style="font-size: 20px"></textarea><br><br>
+                    <button id="refresh">Refresh Animation</button>
+                    <button id="logout">Logout</button>        
+                `
+let animation = document.getElementById("outlet");
+login.innerHTML = animation;
+
+
+
+
